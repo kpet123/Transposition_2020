e0e3f101cc7ea5c9bb2434bacdc655f6878f2234
Kaitlin Pet <kaitlinpet@gmail.com>
2020-05-03 23:20:17 -0400

    updated file strucutre

---
+++ create_environment.sh
@@ -0,0 +1,7 @@
+
+pip install omrdatasettools
+pip install fast_slic
+#conda deactivate
+echo "New environment music-network-env configured."
+echo "Type  'conda activate music-network-env' to run" 
+echo "New ipython kernel 'music-network-kernel' created" 
